4885d3c5edc582ea27aa197a7042d81c562a572c
usernamenah <sujithpappala777@gmail.com>
2024-06-03 10:10:37 +0530

    cors lo origin add chesa and url petta backend dhi

--- index.js
+++ index.js
@@ -11,25 +11,27 @@ const login = require('./Auth_Student');
 const add_lab = require('./create_labs_managaement');
 const login_management = require('./Auth_management');
 
+// CORS configuration
 app.use(cors({
-    origin: 'https://adaan-pradaan-front-egorke4q2-usernamenahs-projects.vercel.app'
+    origin: 'https://adaan-pradaan-front-egorke4q2-usernamenahs-projects.vercel.app',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
 }));
+
 app.use(logger("dev"));
 
 // Middleware
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-
-
-
+// MongoDB connection
 const connectionParams = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 };
 
-
-
-// Connect to MongoDB
 mongoose.connect(process.env.dbUrl, connectionParams)
     .then(() => {
         console.info("Connected to the DB");
@@ -37,10 +39,6 @@ mongoose.connect(process.env.dbUrl, connectionParams)
     .catch((error) => {
         console.error("Error connecting to the DB:", error);
     });
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 
 // Routes
 app.get("/", (req, res) => {
@@ -51,7 +49,66 @@ app.use("/api", login);
 app.use("/api", add_lab);
 app.use("/api", login_management);
 
+// Start server
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
+
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
+// const logger = require("morgan");
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// // Import routes
+// const login = require('./Auth_Student');
+// const add_lab = require('./create_labs_managaement');
+// const login_management = require('./Auth_management');
+
+// app.use(cors({
+//     origin: 'https://adaan-pradaan-front-egorke4q2-usernamenahs-projects.vercel.app'
+// }));
+// app.use(logger("dev"));
+
+// // Middleware
+// app.use(express.json());
+
+
+
+
+// const connectionParams = {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// };
+
+
+
+// // Connect to MongoDB
+// mongoose.connect(process.env.dbUrl, connectionParams)
+//     .then(() => {
+//         console.info("Connected to the DB");
+//     })
+//     .catch((error) => {
+//         console.error("Error connecting to the DB:", error);
+//     });
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+
+// // Routes
+// app.get("/", (req, res) => {
+//   res.send("Adan_Pradhan server is running!");
+// });
+
+// app.use("/api", login);
+// app.use("/api", add_lab);
+// app.use("/api", login_management);
+
+// const port = process.env.PORT || 4000;
+// app.listen(port, () => {
+//     console.log(`Server is running on ${port}`);
+// });
