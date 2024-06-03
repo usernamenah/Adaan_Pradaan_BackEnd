b80f802c1f045bf220e593f6c14d3d62845d2868
usernamenah <sujithpappala777@gmail.com>
2024-06-03 10:15:08 +0530

    cors lo origin add chesa and url petta backend dhi

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors({
 app.use(logger("dev"));
 
 // Middleware
-app.use(express.json());
+// app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
